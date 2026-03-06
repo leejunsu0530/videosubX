4463e66bf7296d47bb89e0e41474628ccb87598b
leejunsu <leejunsu0530@gmail.com>
2026-03-07 00:49:36 +0900

    ydl.evaluate_outtmpl 쓰면 출력 템플릿에 infodict 넣을 수 있음. 구문 강조는 빼고, 그냥 인자 한개식 추가하는 표같은 형식으로 만들어보기. 인자 파싱 후 help 필요

--- test/test.py
+++ test/test.py
@@ -1,15 +1,15 @@
 from yt_dlp import YoutubeDL
-from videosubx.download.parse import parse_cli_args
+# from videosubx.download.parse import parse_cli_args
 from rich.pretty import pprint
 #
 with YoutubeDL() as ydl:
-    api_opts = parse_cli_args(
-        '--format "bv[height<=720]+ba" --output "%(title)s.%(ext)s"')
-    pprint(api_opts)
-    pprint(ydl.prepare_outtmpl("%(title)s.%(ext)s",
-                               {
-                                   "title": "Example Video",
-                                   "ext": "mp4",
-                                   
-                               }))
+    # api_opts = parse_cli_args(
+    # '--format "bv[height<=720]+ba" --output "%(title)s.%(ext)s"')
+    # pprint(api_opts)
+    info_dict = {
+        "title": "Example Video",
+        "ext": "mp4",
+    }
+    outtmpl = "%(title)s.%(ext)s"
+    print(ydl.evaluate_outtmpl(outtmpl, info_dict))
     # ydl.parse_outtmpl()
