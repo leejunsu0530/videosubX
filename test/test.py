31e9b61191ebc141b9aa920842d410367fbe297c
leejunsu <leejunsu0530@gmail.com>
2026-03-10 23:53:34 +0900

    인자 파싱 연구

--- test/test.py
+++ test/test.py
@@ -1,15 +1,7 @@
-from yt_dlp import YoutubeDL
-# from videosubx.download.parse import parse_cli_args
-from rich.pretty import pprint
-#
-with YoutubeDL() as ydl:
-    # api_opts = parse_cli_args(
-    # '--format "bv[height<=720]+ba" --output "%(title)s.%(ext)s"')
-    # pprint(api_opts)
-    info_dict = {
-        "title": "Example Video",
-        "ext": "mp4",
-    }
-    outtmpl = "%(title)s.%(ext)s"
-    print(ydl.evaluate_outtmpl(outtmpl, info_dict))
-    # ydl.parse_outtmpl()
+"""
+Current TODO:
+- ydl.evaluate_outtmpl 쓰면 출력 템플릿에 infodict 넣을 수 있음.
+- 구문 강조는 빼고, 그냥 인자 한개식 추가하는 노션 표같은 형식으로 만들어보기.
+- 인자 파싱 후 help 구현 필요 >> test_ytdlp_args.ipynb로
+- hf hub에서 번역 테스트
+"""
