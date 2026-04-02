71e3442fef460808f01fd057aa3a66e1f382377e
leejunsu <leejunsu0530@gmail.com>
2026-04-02 16:18:25 +0900

    vad 관련

--- videosubx/transcription/base.py
+++ videosubx/transcription/base.py
@@ -6,10 +6,16 @@ import gc
 import torch
 import platform
 import subprocess
-from typing import Literal, Optional, Any
+from typing import Literal, Optional, Any, Generator
 from dataclasses import dataclass
 from pathlib import Path
 import numpy as np
+
+
+# class VadConfig:
+    # pass
+    # device = torch.device("NPU")
+    # 인텔 gpu는 되는데 npu는 안해줌
 
 class TranscriberBase:
     """
@@ -20,9 +26,14 @@ class TranscriberBase:
     - 가비지 컬렉터 부분
     - 로깅 부분?
     - ov base도 만들고, 변환 후 저장해서 다음에는 그거로 불러오게 하기
+
+
+    어디부터 어디까지를 클래스 변수로 하고 어디를 인스턴스 변수로 할지?
     """
 
-    def __init__(self) -> None:
+    def __init__(self,
+                #  vad_config: VadConfig
+                 ) -> None:
         pass
 
     @staticmethod
@@ -30,16 +41,28 @@ class TranscriberBase:
         for obj in objects:
             del obj
         gc.collect()
-        torch.cuda.empty_cache
+        torch.cuda.empty_cache()
 
-    def load_audio(self):
-        pass
+    def load_audio(self,
+                   audio_path_or_url: str | Path,
+                   use_vad: bool = True,
+                   chunk_length_minutes: Optional[float] = None
+                   ) -> Generator[np.ndarray, None, None]:
+        """
+        You can use path(string or pathlib.Path) or url(string)
+        it will process 
+            1. VAD(default, but if you don't use it, sentence can be cut in the middle), 
+            chunking, 
+            and return a generator of audio chunks as numpy arrays.
+        """
+        # 불러오는 방식을 쪼갤지?
+        # 가져온 제너레이터를 self. 변수로 만들기?
 
     def get_audio_duration(self):
         pass
 
-    def download_model(self):
-        pass
+    # def download_model(self):
+        # pass
 
     def transcribe(self):
         pass
@@ -52,6 +75,10 @@ class TranscriberBase:
 
     def diarize(self):
         pass
+
+
+class TranscriberBaseOV(TranscriberBase):
+    pass
 
 
 class VadBase:
