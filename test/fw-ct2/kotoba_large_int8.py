f602410311fc21eb49dcac149d433d367271c0df
leejunsu <leejunsu0530@gmail.com>
2026-04-28 17:20:31 +0900

    변환 명령어에서 저거 빼먹지만 않으면 잘 작동함. 그리고 실 구동시에는 int8로 했음. --copy_files tokenizer.json preprocessor_config.json --quantization float16 정확도는 어떨지 보기

--- test/fw-ct2/kotoba_large_int8.py
+++ test/fw-ct2/kotoba_large_int8.py
@@ -5,13 +5,16 @@ from faster_whisper import WhisperModel, BatchedInferencePipeline
 from time import time
 print("라이브러리 로딩 완료")
 
-MODEL_PATH = r"C:\Users\leeju\Projects\.ct2-workspace\outputs\kotoba-whisper-v2.2-ct2-int8"
+MODEL_PATH = r"C:\Users\leeju\Projects\.ct2-workspace\outputs\kotoba-whisper-v2.2-ct2-int8" # 변환시에 내가 설정 하나를 빼먹어서 설정값이 빠짐.
+MODEL_ID = "kotoba-tech/kotoba-whisper-v2.0-faster" # 일단 코토바가 turbo보다 배는 빠르다!
 FILE = r"C:\Users\leeju\Desktop\test_15min.mp4"
 # FILE2 = r"C:\Users\leeju\Desktop\[일본 파스텔] 신주쿠 던전에서 당황한 나나 님... 한일 지하철의 추억 + 쿠키 (한일자막).mkv"
 # 샤갈 일본어가 아니잖아
 
 _srt = time()
-model = WhisperModel("turbo", device="cpu", compute_type="int8")
+model = WhisperModel(  # MODEL_PATH,
+    MODEL_ID,
+    device="cpu", compute_type="int8")
 _model_loaded = time()
 print(f"모델 로딩: {_model_loaded-_srt:.3f}")
 
--- test/fw-ct2/wx_test_kotoba_int8.py
+++ test/fw-ct2/wx_test_kotoba_int8.py
@@ -1,3 +1,73 @@
 
+import whisperx
+# from whisperx.asr import WhisperModel
+import gc
+# from whisperx.diarize import DiarizationPipeline
+import torch
+from shutil import which
+import os
+from pathlib import Path
+
+ffmpeg_path = which("ffmpeg")
+if ffmpeg_path is None:
+    raise RuntimeError("ffmpeg is not installed or not found in PATH.")
+os.add_dll_directory(str(Path(ffmpeg_path).parent))
+
 MODEL_PATH = r"C:\Users\leeju\Projects\.ct2-workspace\outputs\kotoba-whisper-v2.2-ct2-int8"
-FILE = r"C:\Users\leeju\Desktop\test_15min.mp4"
+MODEL_ID = "kotoba-tech/kotoba-whisper-v2.0-faster"
+
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+audio_file = r"C:\Users\leeju\Desktop\test_15min.mp4"
+batch_size = 1  # 16 # reduce if low on GPU mem
+# change to "int8" if low on GPU mem (may reduce accuracy)
+compute_type = "int8"  # "float16"
+
+print("모델 로딩")
+# 1. Transcribe with original whisper (batched)
+model = whisperx.load_model(
+    # "large-v2",
+    # MODEL_PATH,
+    MODEL_ID,
+    device, compute_type=compute_type,
+    language='ja'
+)
+
+# save model to local path (optional)
+# model_dir = "/path/"
+# model = whisperx.load_model("large-v2", device, compute_type=compute_type, download_root=model_dir)
+
+audio = whisperx.load_audio(audio_file)
+print("오디오 로딩 완료")
+result = model.transcribe(audio, batch_size=batch_size,
+                          print_progress=True)
+print(result["segments"])  # before alignment
+
+# delete model if low on GPU resources
+gc.collect()
+torch.cuda.empty_cache()
+del model
+
+# 2. Align whisper output
+model_a, metadata = whisperx.load_align_model(
+    language_code=result["language"], device=device)
+result = whisperx.align(result["segments"], model_a,
+                        metadata, audio, device, return_char_alignments=False)
+
+print(result["segments"])  # after alignment
+
+# delete model if low on GPU resources
+gc.collect()
+torch.cuda.empty_cache()
+del model_a
+
+# 3. Assign speaker labels
+# diarize_model = DiarizationPipeline(token=YOUR_HF_TOKEN, device=device)
+
+# add min/max number of speakers if known
+# diarize_segments = diarize_model(audio)
+# diarize_model(audio, min_speakers=min_speakers, max_speakers=max_speakers)
+
+# result = whisperx.assign_word_speakers(diarize_segments, result)
+# print(diarize_segments)
+# print(result["segments"])  # segments are now assigned speaker IDs
